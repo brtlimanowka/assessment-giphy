b2cc7844665fcb8bf98805514d1212b446076928
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-10-02 16:14:15 +0200

    Style ErrorCard

--- src/components/ui/ErrorCard/ErrorCard.js
+++ src/components/ui/ErrorCard/ErrorCard.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ErrorCardStyled from './ErrorCard.styled';
 
-const ErrorCard = () => {
-  return <ErrorCardStyled>ErrorCard</ErrorCardStyled>;
+const ErrorCard = ({ errorMessage }) => {
+  return (
+    <ErrorCardStyled>
+      <h1>Something went wrong</h1>
+      <p>
+        <label>Error message: </label>
+        <span>{errorMessage}</span>
+      </p>
+    </ErrorCardStyled>
+  );
 };
 
 export default ErrorCard;
--- src/components/ui/ErrorCard/ErrorCard.styled.js
+++ src/components/ui/ErrorCard/ErrorCard.styled.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
-const ErrorCardStyled = styled.main``;
+const ErrorCardStyled = styled.main`
+  width: 300px;
+  margin: 10px auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  border-left: 5px solid ${(props) => props.theme.colors.errorBackground};
+  h1 {
+    font-size: 220%;
+  }
+  p {
+    font-size: 160%;
+    span {
+      border-bottom: 1px solid ${(props) => props.theme.colors.errorBackground};
+    }
+  }
+`;
 
 export default ErrorCardStyled;
