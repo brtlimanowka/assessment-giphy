002ca6791fffc1dc9107288c2fc9b822fc156f59
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-10-02 13:44:44 +0200

    Corret token

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -9,14 +9,14 @@ describe('queryBuilder', () => {
   test('returns a valid query with default offset', () => {
     const builtQuery = queryBuilder('kittens');
     expect(builtQuery).toBe(
-      'https://api.giphy.com/v1/gifs/search?z3TCxWMXI3poet0DNQBeC8RfYrprX7U1,&q=kittens&offset=0&limit=5&rating=G&lang=en'
+      'https://api.giphy.com/v1/gifs/search?z3TCxWMXI3poet0DNQBeC8RfYrprX7U1&q=kittens&offset=0&limit=5&rating=G&lang=en'
     );
   });
 
   test('returns a valid query with custom offset', () => {
     const builtQuery = queryBuilder('kittens', 1);
     expect(builtQuery).toBe(
-      'https://api.giphy.com/v1/gifs/search?z3TCxWMXI3poet0DNQBeC8RfYrprX7U1,&q=kittens&offset=1&limit=5&rating=G&lang=en'
+      'https://api.giphy.com/v1/gifs/search?z3TCxWMXI3poet0DNQBeC8RfYrprX7U1&q=kittens&offset=1&limit=5&rating=G&lang=en'
     );
   });
 });
